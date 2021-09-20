043bef95854e170570ae306e46d01bd2d11fdc83
Tetiana Grek <tetiana_grek@epam.com>
2021-09-21 00:22:32 +0300

    constructor fix

--- 01_oop_and_ood/src/Point.ts
+++ 01_oop_and_ood/src/Point.ts
@@ -7,7 +7,7 @@ export class Point {
   private readonly x: number;
   private readonly y: number;
 
-  constructor(x, y) {
+  constructor(x: number = 0, y: number = 0) {
     this.x = x;
     this.y = y;
   }
